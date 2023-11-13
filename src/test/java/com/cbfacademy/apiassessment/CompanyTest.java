93ea3ba42fcdb0dfc5c7391e97735a5a7302e1b4
Senam <senamdaniel@gmail.com>
2023-11-13 21:55:41 +0000

    added more tests to the company test class

--- src/test/java/com/cbfacademy/apiassessment/CompanyTest.java
+++ src/test/java/com/cbfacademy/apiassessment/CompanyTest.java
@@ -4,19 +4,28 @@ import com.cbfacademy.apiassessment.ExceptionClasses.InsufficientFundsException;
 import com.cbfacademy.apiassessment.ExceptionClasses.InvalidActionException;
 import com.cbfacademy.apiassessment.FinTechClasses.Company;
 import com.cbfacademy.apiassessment.FinTechClasses.Game;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperties;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CompanyTest {
     //need to find a way to test the investment method - the problem is that the method provides random results
 
+    private Game game;
+    private Company company;
+    @BeforeEach
+    public void gameAndCompanyInitialiser(){
+        game = new Game();
+        company = game.getCompany();
+    }
+
     @Test
     @DisplayName("Testing crowdFund method increases revenue by 500000 in Company ")
     public void testCrowdFund() throws InvalidActionException {
-        Game game = new Game();
-        Company company = game.getCompany();
+
         double initialRevenue = company.getRevenue();
 
         company.crowdFund();
@@ -28,9 +37,9 @@ public class CompanyTest {
 
 
     @Test
-    @DisplayName("Testing insufficient funds")
+    @DisplayName("Testing has sufficient funds method")
     public void testInsufficientFunds(){
-        Game game = new Game();
+//        Game game = new Game();
         Company company = game.getCompany();
         boolean result = company.hasSufficientFunds(1000);
 
@@ -39,7 +48,7 @@ public class CompanyTest {
     }
 
     @Test
-    @DisplayName("Testing that the addEmployee method throws an exception")
+    @DisplayName("Testing that the addEmployee method throws an Insufficient funds exception")
     public void testAddEmployeeException() throws InsufficientFundsException {
         Game game = new Game();
         Company company = game.getCompany();
@@ -64,15 +73,65 @@ public class CompanyTest {
 
     }
 
+
     @Test
-    @DisplayName("Testing method increases departments in company ")
-    public void testAddDepartment() throws InsufficientFundsException, InvalidActionException {
+    @DisplayName("Testing the removeEmployee method removes employees and departments")
+    public void testRemoveEmployee() throws InvalidActionException {
         Game game = new Game();
         Company company = game.getCompany();
+        company.setEmployees(20);
+        company.setDepartments(2);
         int initDepartments = company.getDepartments();
 
-        company.addEmployee(10);
+        company.removeEmployee(20);
 
+        int newDepartments = company.getDepartments();
+
+        assertEquals(initDepartments - 2, newDepartments);
+    }
+    @Test
+    @DisplayName("Testing the removeEmployee method increases revenue")
+    public void testRemoveEmployeeIncreasesRevenue() throws InvalidActionException {
+//        Game game = new Game();
+//        Company company = game.getCompany();
+        company.setEmployees(20);
+
+        double initRevenue = company.getRevenue();
+
+        company.removeEmployee(1);
+
+        double newRevenue = company.getRevenue();
+
+        assertEquals(initRevenue + 5000, newRevenue);
+    }
+
+
+    @Test
+    @DisplayName("Testing the removeEmployee method throws an exception")
+    public void testRemoveEmployeeThrowsException() throws InvalidActionException{
+        company.setEmployees(5);
+
+        assertThrows(InvalidActionException.class, () -> company.removeEmployee(6));
+
+    }
+
+    @Test
+    @DisplayName("Testing customerRevenueBoost method increases revenue")
+    public void testCustomerRevenueBoostIncreasesRevenue(){
+        double initRevenue = company.getRevenue();
+        company.setCustomerBase(50);
+        company.customerRevenueBoost();
+        double newRevenue = company.getRevenue();
+
+        assertEquals(initRevenue + 250, newRevenue);
+
+    }
+
+    @Test
+    @DisplayName("Testing add department method increases departments in company ")
+    public void testAddDepartment() throws InsufficientFundsException, InvalidActionException {
+        int initDepartments = company.getDepartments();
+        company.addEmployee(10);
         company.addDepartment();
         int newDepartments = company.getDepartments();
 
@@ -83,8 +142,6 @@ public class CompanyTest {
     @Test
     @DisplayName("Testing researchAndDev method increases in productXP ")
     public void testResearchAndDev() throws InvalidActionException {
-        Game game = new Game();
-        Company company = game.getCompany();
         int initProductXP = company.getProductXP();
         double initRevenue = company.getRevenue();
 
@@ -141,8 +198,6 @@ public class CompanyTest {
     @Test
     @DisplayName("Testing marketing method increases customerBase by 1000 and reduces revenue by 10000 ")
     public void testMarketing() throws InvalidActionException {
-        Game game = new Game();
-        Company company = game.getCompany();
         int initCustomerBase = company.getCustomerBase();
         double initRevenue = company.getRevenue();
 
@@ -160,9 +215,6 @@ public class CompanyTest {
     @Test
     @DisplayName("Testing the marketing method throws an exception if revenue is less than 10000")
     public void testMarketingException() throws InvalidActionException {
-        Game game = new Game();
-        Company company = game.getCompany();
-
         company.setRevenue(1000);
 
         assertThrows(InvalidActionException.class, () -> company.marketing());
@@ -171,20 +223,32 @@ public class CompanyTest {
     }
 
 
-    //need to add test for if revenue is insufficient
+    @Test
+    @DisplayName("Testing sniper invest method changes revenue")
+    public void testSniperInvestMethod() throws InvalidActionException {
+        double initRevenue = company.getRevenue();
+        company.sniperInvestment();
+        double newRevenue = company.getRevenue();
 
-//    @Test
-//    @DisplayName("Testing invest method")
+        assertNotEquals(initRevenue, newRevenue);
+    }
+
+
+    @Test
+    @DisplayName("Testing passive invest method changes revenue")
+    public void testPassiveInvestMethod() throws InvalidActionException {
+        double initRevenue = company.getRevenue();
+        company.passiveInvestment();
+        double newRevenue = company.getRevenue();
+
+        assertNotEquals(initRevenue, newRevenue);
+    }
 
     @Test
     @DisplayName("Testing increment crowdFund method")
     public void testIncrementCrowdFund(){
-        Game game = new Game();
-        Company company = game.getCompany();
         int initCrowdFund = company.getCrowdFundCount();
-
         company.incrementCrowdFundCount();
-
         int newCrowdFund = company.getCrowdFundCount();
 
         assertEquals(initCrowdFund + 1, newCrowdFund);
@@ -194,16 +258,14 @@ public class CompanyTest {
     @Test
     @DisplayName("Testing reset crowdFund Count resets crowdFundCount to 0")
     public void testResetCrowdFund(){
-        Game game = new Game();
-        Company company = game.getCompany();
         int initCrowdFund = company.getCrowdFundCount();
-
         company.incrementCrowdFundCount();
-
         company.resetCrowdFundCount();
 
         assertEquals(initCrowdFund, 0);
     }
+
+    //Need a resetinvestcount test - start from here when making more tests 
 
     @Test
     @DisplayName("Testing reduceRevenue method reduces revenue by inputted percentage")
@@ -277,36 +339,6 @@ public class CompanyTest {
         assertEquals(initCustomerBase - 5, newCustomerBase);
 
     }
-
-
-    @Test
-    @DisplayName("Testing the removeEmployee method removes employees and departments")
-    public void testRemoveEmployee() throws InvalidActionException {
-        Game game = new Game();
-        Company company = game.getCompany();
-        company.setEmployees(20);
-        company.setDepartments(2);
-        int initDepartments = company.getDepartments();
-
-        company.removeEmployee(20);
-
-        int newDepartments = company.getDepartments();
-
-        assertEquals(initDepartments - 2, newDepartments);
-    }
-
-
-    @Test
-    @DisplayName("Testing the removeEmployee method throws an exception")
-    public void testRemoveEmployeeThrowsException() throws InvalidActionException{
-        Game game = new Game();
-        Company company = game.getCompany();
-        company.setEmployees(5);
-
-        assertThrows(InvalidActionException.class, () -> company.removeEmployee(6));
-
-    }
-
 
     @Test
     @DisplayName("Testing the customerRevenueBoost method increases the revenue by 5 * customerBase")
